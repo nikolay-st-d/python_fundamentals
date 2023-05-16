f2b90b45816f8d990d13bce2099a2b73a4b897d5
Nikolay-St-D <126271396+Nikolay-St-D@users.noreply.github.com>
2023-05-17 00:56:49 +0300

    next_happy_year resolved

--- data_types_and_variables/next_happy_year.py
+++ data_types_and_variables/next_happy_year.py
@@ -1,24 +1,18 @@
 number = int(input())
-next_num = number
+num = number
 
 while True:
     found = True
-    next_num += 1
-    next_num_to_str = str(next_num)
-    num_len = len(next_num_to_str)
-    last_digit = 0
-    new_num = ''
-    for i in range(num_len):
-        digit = int(next_num_to_str[i])
-        if digit == last_digit:
+    count = 0
+    num += 1
+    num_to_str = str(num)
+    for digit in range(10):
+        count = num_to_str.count(str(digit))
+        if count > 1:
             found = False
             break
-        else:
-            last_digit = digit
-            new_num += str(digit)
-
     if found:
-        print(new_num)
+        print(num_to_str)
         break
 
 
